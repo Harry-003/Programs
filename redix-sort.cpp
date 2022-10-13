3774e8f1ad9b79255267f52be6d85e68024e2332
Agrim Kaundal <agrim3502@gmail.com>
2022-10-13 14:44:58 +0530

    Added Radix sorting Algo

---
+++ redix-sort.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int getMax(vector<int> arr, int n)
+{
+	int mx = arr[0];
+	for (int i = 1; i < n; i++)
+		if (arr[i] > mx)
+			mx = arr[i];
+	return mx;
+}
+void countSort(vector<int> arr, int n, int exp)
+{
+	int output[n];
+	int i, count[10] = { 0 };
+	for (i = 0; i < n; i++)
+		count[(arr[i] / exp) % 10]++;
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+	for (i = n - 1; i >= 0; i--) {
+		output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+		count[(arr[i] / exp) % 10]--;
+	}
+	for (i = 0; i < n; i++)
+		arr[i] = output[i];
+}
+
+void radixsort(vector<int> arr, int n)
+{
+	int m = getMax(arr, n);
+	for (int exp = 1; m / exp > 0; exp *= 10)
+		countSort(arr, n, exp);
+}
+
+void print(vector<int> arr, int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+}
+
+int main()
+{
+	vector<int> arr;
+    cout<<"Enter the size of the array : ";
+    int s;
+    cin>>s;
+    cout<<"Enter the elements of the array seprated by space >>>>"<<endl;
+    for(int i  =0 ;i<s;i++){
+        int b;
+        cin>>b;
+        arr.push_back(b);
+    }
+	radixsort(arr, arr.size());
+	print(arr, arr.size());
+	return 0;
+}
