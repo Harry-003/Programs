0d1d778d29130284a18dcde5ae284b50a00851c8
Aman Bhavsar <96308303+5264A@users.noreply.github.com>
2022-10-14 22:46:32 +0530

    Update binarysearch_recursion.cpp

--- Binary_Search/binarysearch_recursion.cpp
+++ Binary_Search/binarysearch_recursion.cpp
@@ -1,28 +1,58 @@
 #include <iostream>
 using namespace std;
-void BS(int arr[],int search,int start,int end){
-    if(start>end){
-        cout<<"Element is not found in an array";
-        return;
-    }
-    int mid=(start+end)/2;
-    if(arr[mid]==search){
-        cout<<"Element is found in an array";
-        return;
-    }
-    if(arr[mid]<search) BS(arr,search,mid+1,end);
-    if(arr[mid]>search) BS(arr,search,start,mid-1);
+
+int BinarySearch(int arr[], int num, int beg, int end)
+{
+ int mid;
+ 
+ if (beg > end){
+  
+  cout << "Number is not found";
+  return 0;
+  
+ } else {
+  
+  mid = (beg + end) / 2;
+  
+  if(arr[mid] == num){
+   
+   cout << "Number is found at " << mid << " index \n";
+   return 0;
+  
+  } else if (num > arr[mid]) {
+   
+   BinarySearch (arr, num, mid+1, end);
+   
+  } else if (num < arr[mid]) {
+   
+   BinarySearch (arr, num, beg , mid-1);
+  }
+ }
+ 
+ 
 }
-int main(){
-    int n,start=0,end,mid,search;
-    cout<<"Enter the size of an array :";
-    cin>>n;
-    int arr[n];
-    end=n-1;
-    cout<<"\nEnter the elements into an array:";
-    for(int i=0;i<n;i++) cin>>arr[i];
-    cout<<"Enter the element that need to be searched:";
-    cin>>search;
-    BS(arr,search,start,end);
-    return 0;
+ 
+int main() {
+ 
+ int arr[100], num, i, n, beg, end;
+ 
+ cout <<"Enter the size of an array (Max 100) \n";
+ cin >> n;
+ 
+ cout <<"Enter the sorted values \n";
+ 
+ for(i=0; i<n; i++) {
+  
+  cin >> arr[i];
+ }
+ 
+ cout <<"Enter a value to be search \n";
+ cin >> num;
+ 
+ beg = 0;
+ end = n-1;
+ 
+ BinarySearch (arr, num, beg, end);
+ 
+ return 0;
 }
