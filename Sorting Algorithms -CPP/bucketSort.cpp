0d8340a52a21f3c904dc1a2de24976709536644d
Aman Bhavsar <96308303+5264A@users.noreply.github.com>
2022-10-14 22:42:13 +0530

    Update bucketSort.cpp

--- Sorting Algorithms -CPP/bucketSort.cpp
+++ Sorting Algorithms -CPP/bucketSort.cpp
@@ -1,47 +1,46 @@
-#include<bits/stdc++.h>
+// C++ program to sort an 
+// array using bucket sort
+#include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
-
-int findMax(int A[],int n)
+  
+// Function to sort arr[] of 
+// size n using bucket sort
+void bucketSort(float arr[], int n)
 {
-	int max = INT_MIN;
-	for(int i=0;i<n;i++)
-	{
-	if(A[i]>max)
-	max=A[i];
-	}
-	return max;
+      
+    // 1) Create n empty buckets
+    vector<float> b[n];
+  
+    // 2) Put array elements 
+    // in different buckets
+    for (int i = 0; i < n; i++) {
+        int bi = n * arr[i]; // Index in bucket
+        b[bi].push_back(arr[i]);
+    }
+  
+    // 3) Sort individual buckets
+    for (int i = 0; i < n; i++)
+        sort(b[i].begin(), b[i].end());
+  
+    // 4) Concatenate all buckets into arr[]
+    int index = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < b[i].size(); j++)
+            arr[index++] = b[i][j];
 }
-
-void bucketSort(int a[], int n){
-	int max = findMax(a, n);
-	int bucket[max],i;
-	
-	for(int i=0;i<=max;i++){
-		bucket[i]= 0;
-	}
-	
-	//bin-array
-	for(int i=0;i<n;i++){
-	    bucket[a[i]]++;
-	}
-	
-	for(int i=0, j=0; i<=max;i++){
-		while(bucket[i]>0){
-			a[j++]=i;
-			bucket[i]--;
-		}
-	}
-}
-
+  
+/* Driver program to test above function */
 int main()
 {
-	int n=8;
-    int arr[n] = {2, 5, 8, 12, 3, 6, 7, 10};
-    
+    float arr[]
+        = { 0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434 };
+    int n = sizeof(arr) / sizeof(arr[0]);
     bucketSort(arr, n);
-    for(int i=0;i<n;i++){
-    	cout<<arr[i]<<" ";
-	}
- 
+  
+    cout << "Sorted array is \n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
     return 0;
 }
