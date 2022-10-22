ee1f6633348800755a0d9c5f852dea62d836812f
Sunera Sunilaka <34810235+Sunera-2216@users.noreply.github.com>
2022-10-22 19:27:02 +0530

    Create BubbleSort.java

---
+++ Sorting Algorithms/BubbleSort.java
@@ -0,0 +1,39 @@
+public class BubbleSort 
+{
+  
+    public static void main(String[] args)
+    {
+        Sort ob = new Sort();
+        int arr[] = {5, 6, 5, 12, 2, 11, 9};    
+        bubbleSort(arr, arr.length);
+        ob.display(arr);
+    }
+  
+    static void bubbleSort(int arr[], int n)
+    {                                       
+        if (n == 1)                     //passes are done
+        {
+            return;
+        }
+
+        for (int i = 0; i < n-1; i++)       //iteration through unsorted elements
+        {
+            if (arr[i] > arr[i+1])      //check if the elements are in order
+            {                           //if not, swap them
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+            
+        bubbleSort(arr, n-1);           //one pass done, proceed to the next
+    }
+
+    void display(int arr[])                 //display the array
+    {  
+        for (int i = 0; i < arr.length; ++i) 
+        {
+            System.out.print(arr[i] + " ");
+        } 
+    } 
+}
